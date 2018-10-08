a5f4a2bcc426a1bb56d5ebaf9c7f00633423d9b0
Doruk Kutlu <dorukk@gmail.com>
2018-10-09 00:27:02 +0300

    use absolute models folder path

--- src/models/index.js
+++ src/models/index.js
@@ -3,8 +3,8 @@ import path from "path";
 import USERS from "./users.json";
 import EVENTS from "./events.json";
 
-export const EVENTS_DB = path.resolve(__dirname, "events.json");
-export const USERS_DB = path.resolve(__dirname, "users.json");
+export const EVENTS_DB = path.join(__dirname, "events.json");
+export const USERS_DB = path.join(__dirname, "users.json");
 
 export default {
   users: USERS,
