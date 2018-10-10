f0e34f2036772d342db3b945b11fb01b0992f40f
Doruk Kutlu <dorukk@gmail.com>
2018-10-10 20:13:49 +0300

    modularize apollo server init

--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
 import "dotenv/config";
 import chalk from "chalk";
 import express from "express";
-import { ApolloServer } from "apollo-server-express";
 
+import registerGqlServer from "./server.js";
 import db from "./db";
-import typeDefs from "./schema";
-import resolvers from "./resolvers";
-
 const app = express();
-
-new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => ({
-    models: db.models,
-    me: (await db.models.user.findAll({ limit: 1 }))[0]
-  }),
-  formatError: error => {
-    const message = error.message
-      .replace("SequelizeValidationError: ", "")
-      .replace('Validation error: ', "");
-
-    return {
-      ...error,
-      message,
-    };
-  },
-}).applyMiddleware({ app, path: "/gql" });
 
 db
   .sync({ force: Boolean(process.env.SYNC) })
   .then(async () => {
     const port = Number(process.env.PORT) || 8000;
+    const me = await db.models.User.findAll({ limit: 1 })[0];
+    const context = async ({ req }) => ({
+      models: db.models,
+      me,
+    });
+
+    registerGqlServer(app, context);
     await app.listen({ port });
 
     console.log(  // eslint-disable-line
---
+++ src/server.js
@@ -0,0 +1,21 @@
+import { ApolloServer } from "apollo-server-express";
+
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+
+export default (app, context) =>
+  new ApolloServer({
+    typeDefs,
+    resolvers,
+    context,
+    formatError: error => {
+      const message = error.message
+        .replace("SequelizeValidationError: ", "")
+        .replace("Validation error: ", "");
+
+      return {
+        ...error,
+        message
+      };
+    }
+  }).applyMiddleware({ app, path: "/gql" });
