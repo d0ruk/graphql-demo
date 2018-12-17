73351d5f27e0d3dd63badfee1382b446ec7196cb
Doruk Kutlu <dorukk@gmail.com>
2018-12-17 05:47:27 +0300

    wrap seed.js in cli

--- seed.js
+++ seed.js
@@ -3,9 +3,9 @@ import "dotenv/config";
 import faker from "faker";
 import { range, sampleSize, random } from "lodash";
 import chalk from "chalk";
+import meow from "meow";
 
 import db from "./src/db";
-const ROWS = 100;
 const ATTENDING = 5;
 
 const admin = {
@@ -15,68 +15,91 @@ const admin = {
   role: "ADMIN",
 };
 
-db.sync({ force: true })
-  .then(() =>
-    Promise.all([
-      Promise.all(createUsers(ROWS)),
-      Promise.all(createEvents(ROWS)),
-    ])
-  )
-  .then(([users, events]) => {
-    const userEvents = Array.from(users, user =>
-      user.addEvents(sampleSize(events, ATTENDING))
-    );
+const cli = meow(
+  `
+	Options
+	  --rows, -r
 
-    const eventOwner = Array.from(events, event => {
-      const [owner] = sampleSize(users, 1);
-      return event.setOwner(owner.username);
-    });
+	Examples
+	  $ seed -r 5
+`,
+  {
+    flags: {
+      rows: {
+        type: "number",
+        alias: "r",
+      },
+    },
+    description: false,
+  }
+);
 
-    return Promise.all([...eventOwner, ...userEvents]);
-  })
-  .then(() => db.models.User.create(admin))
+run(db, cli.flags)
   .then(() => {
     console.log(
       chalk.green.bold("Successfully seeded database " + process.env.DB_NAME)
     );
-
-    return db.close();
   })
   .catch(err => {
     console.error(chalk.red.bold(err));
     process.exit(42);
   });
 
-function createUsers(n) {
-  return range(n).map(idx =>
-    db.models.User.create({
-      username: faker.lorem.word() + idx,
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-      firstname: faker.name.firstName(),
-      lastname: faker.name.lastName(),
-      picture: faker.image.imageUrl(),
-      age: faker.random.number(),
-      phone: faker.phone.phoneNumber(),
-      bio: faker.lorem.paragraph(),
-    })
-  );
-}
+function run(db, { rows = 100 }) {
+  return db
+    .sync({ force: true })
+    .then(() =>
+      Promise.all([
+        Promise.all(createUsers(rows)),
+        Promise.all(createEvents(rows)),
+      ])
+    )
+    .then(([users, events]) => {
+      const userEvents = Array.from(users, user =>
+        user.addEvents(sampleSize(events, ATTENDING))
+      );
 
-function createEvents(n) {
-  return range(n).map(() =>
-    db.models.Event.create({
-      name: faker.lorem.word(),
-      date: faker.date
-        .future()
-        .toISOString()
-        .substring(0, 10),
-      country: faker.address.countryCode(),
-      city: faker.address.city(),
-      description: faker.lorem.paragraph(),
-      createdAt: dateInPastWeek(),
+      const eventOwner = Array.from(events, event => {
+        const [owner] = sampleSize(users, 1);
+        return event.setOwner(owner.username);
+      });
+
+      return Promise.all([...eventOwner, ...userEvents]);
     })
-  );
+    .then(() => db.models.User.create(admin))
+    .then(() => db.close());
+
+  function createUsers(n) {
+    return range(n).map(idx =>
+      db.models.User.create({
+        username: faker.lorem.word() + idx,
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        firstname: faker.name.firstName(),
+        lastname: faker.name.lastName(),
+        picture: faker.image.imageUrl(),
+        age: faker.random.number(),
+        phone: faker.phone.phoneNumber(),
+        bio: faker.lorem.paragraph(),
+      })
+    );
+  }
+
+  function createEvents(n) {
+    return range(n).map(() =>
+      db.models.Event.create({
+        name: faker.lorem.word(),
+        date: faker.date
+          .future()
+          .toISOString()
+          .substring(0, 10),
+        country: faker.address.countryCode(),
+        city: faker.address.city(),
+        description: faker.lorem.paragraph(),
+        createdAt: dateInPastWeek(),
+      })
+    );
+  }
 }
 
 function dateInPastWeek() {
